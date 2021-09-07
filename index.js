cdc27a8a5705989d5873f9eaac25a80063fca488
Mistakd <56619664+RealMistakd@users.noreply.github.com>
2021-09-07 19:17:25 -0400

    lots of changes

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 //for definitions on click
 function thereDef() {
-	alert(
-		"adverb: 1. in, at, or to that place or position. 2. used in attracting someone's attention or calling attention to someone or something."
-	);
+  alert(
+    "adverb: 1. in, at, or to that place or position. 2. used in attracting someone's attention or calling attention to someone or something."
+  );
 }
 
 function humanDef() {
-	alert("noun: a human being, especially a person as distinguished from an animal or (in science fiction) an alien.");
+  alert(
+    "noun: a human being, especially a person as distinguished from an animal or (in science fiction) an alien."
+  );
 }
 
 function helloDef() {
-	alert("exclaimation: used as a greeting or to begin a phone conversation.");
+  alert("exclaimation: used as a greeting or to begin a phone conversation.");
 }
 
 function welcomeDef() {
-	alert("exclaimation: used to greet someone in a glad or friendly way.");
+  alert("exclaimation: used to greet someone in a glad or friendly way.");
 }
 
 function toDef() {
-	alert("preposition: expressing motion in the direction of (a particular location).");
+  alert(
+    "preposition: expressing motion in the direction of (a particular location)."
+  );
 }
 
 function myDef() {
-	alert("possesive pronoun: belonging to or associated with the speaker.");
+  alert("possesive pronoun: belonging to or associated with the speaker.");
 }
 
 function playgroundDef() {
-	alert("noun: a place where a particular group of people choose to enjoy themselves.");
+  alert(
+    "noun: a place where a particular group of people choose to enjoy themselves."
+  );
 }
 
 function changeContent() {
-	alert("test");
+  document.getElementById("nothing").innerText = "something?";
 }
+
+setTimeout(function transparent() {
+  let v = document.getElementById("id");
+  v.style.color = "transparent";
+}, 5000);
